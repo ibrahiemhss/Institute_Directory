7a1513e02dda500e2decc35afe18d4b9fe4fdcf9
ibrahim <kekoreko3@gmail.com>
2018-11-29 18:38:44 +0200

    designing

---
+++ app/src/main/java/com/directory/institute/institute_directory/DetailsActivity.java
@@ -0,0 +1,153 @@
+package com.directory.institute.institute_directory;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import com.github.barteksc.pdfviewer.PDFView;
+import com.github.barteksc.pdfviewer.scroll.DefaultScrollHandle;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    private static final String STATE_ASSET = "asset";
+    private static final String STATE_PICKED = "picked";
+    private String chosenAsset = null;
+    private Uri pickedDocument = null;
+
+    public final static String EXRA_PDF_VALUES = "extra_pdf";
+
+    @SuppressWarnings("WeakerAccess")
+    @BindView(R.id.detailstoolbar)
+    protected Toolbar mToolbar;
+
+    @SuppressWarnings("WeakerAccess")
+    @BindView(R.id.viewer)
+    protected PDFView viewer;
+
+    private String mAsset_pdf = "profile_of_institute.pdf";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(mToolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+            mToolbar.setTitle(getResources().getString(R.string.app_name));
+        }
+        Bundle extras = getIntent().getExtras();
+        assert extras != null;
+
+        if (extras.containsKey(EXRA_PDF_VALUES)) {
+
+
+            switch (extras.getInt(EXRA_PDF_VALUES)) {
+                case 1:
+                    mAsset_pdf = "profile_of_institute.pdf";
+
+                    break;
+
+                case 2:
+                    mAsset_pdf = "series_manager_of_the_institute.pdf";
+
+                    break;
+                case 3:
+
+                    break;
+
+                case 4:
+
+                    break;
+
+                case 5:
+
+                    break;
+
+                case 6:
+                  mAsset_pdf = "studying_and_exam_department.pdf";
+
+                    break;
+
+                case 7:
+                    mAsset_pdf = "Registry_and_acceptancy_department.pdf";
+
+                    break;
+
+                case 8:
+                    mAsset_pdf = "Graduated_depurtment.pdf";
+
+                    break;
+
+                case 9:
+                    mAsset_pdf = "activity_and_student_departmant_affairs.pdf";
+
+                    break;
+
+                case 10:
+                    mAsset_pdf = "around_application.pdf";
+
+                    break;
+
+
+            }
+
+            loadPdf(mAsset_pdf);
+
+        }
+
+    }
+
+
+    @SuppressWarnings("SameReturnValue")
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                Intent intent = new Intent(DetailsActivity.this, MainActivity.class);
+
+                startActivity(intent);
+                return true;
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putString(STATE_ASSET, chosenAsset);
+        outState.putParcelable(STATE_PICKED, pickedDocument);
+    }
+
+
+    private void loadPdf(String name) {
+        chosenAsset = name;
+        pickedDocument = null;
+        configureViewer(viewer.fromAsset(name));
+    }
+
+    private void configureViewer(PDFView.Configurator configurator) {
+        configurator
+                .enableSwipe(true)
+                .swipeHorizontal(false)
+                .enableDoubletap(true)
+                .scrollHandle(new DefaultScrollHandle(this))
+                .load();
+    }
+
+
+}
+
--- app/src/main/java/com/directory/institute/institute_directory/MainActivity.java
+++ app/src/main/java/com/directory/institute/institute_directory/MainActivity.java
@@ -1,6 +1,11 @@
 package com.directory.institute.institute_directory;
 
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -9,12 +14,21 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import de.hdodenhof.circleimageview.CircleImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+    private final static String TAG = MainActivity.class.getSimpleName();
 
     @SuppressWarnings("WeakerAccess")
     @BindView(R.id.drawer_layout)
@@ -25,6 +39,9 @@ public class MainActivity extends AppCompatActivity {
     @SuppressWarnings("WeakerAccess")
     @BindView(R.id.nav_view)
     protected NavigationView mNavigationView;
+    private Boolean isOpen1=false,isOpen2=false;
+    private Intent intent;
+    private Bundle extras;
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
@@ -32,7 +49,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-
+        isOpen1=false;
+        isOpen2=false;
         setUpToolbar();
 
 
@@ -51,6 +69,9 @@ public class MainActivity extends AppCompatActivity {
         mDrawerLayout.addDrawerListener(toggle);
         toggle.syncState();
 
+        mNavigationView.setNavigationItemSelectedListener(this);
+       // initNavigationDrawer(this,mNavigationView);
+
     }
 
     @Override
@@ -60,8 +81,120 @@ public class MainActivity extends AppCompatActivity {
             mDrawerLayout.closeDrawer(GravityCompat.START);
         } else {
             super.onBackPressed();
+            onBackPressed();
+
+            finish();
         }
     }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        // Handle navigation view item clicks here.
+        switch (item.getItemId()) {
+            case R.id.txt_Institute:
+                 intent = new Intent(MainActivity.this, DetailsActivity.class);
+                 extras = new Bundle();
+
+                extras.putInt(DetailsActivity.EXRA_PDF_VALUES,1);
+                intent.putExtras(extras);
+                startActivity(intent);
+
+                break;
+
+
+
+            case R.id.txt_Directors:
+                intent = new Intent(MainActivity.this, DetailsActivity.class);
+                extras = new Bundle();
+
+                extras.putInt(DetailsActivity.EXRA_PDF_VALUES,2);
+                intent.putExtras(extras);
+                startActivity(intent);
+                break;
+
+            case R.id.txt_Certifications_Section:
+                intent = new Intent(MainActivity.this, DetailsActivity.class);
+                extras = new Bundle();
+
+                extras.putInt(DetailsActivity.EXRA_PDF_VALUES,3);
+                intent.putExtras(extras);
+            //    startActivity(intent);
+                break;
+
+            case R.id.txt_library:
+                intent = new Intent(MainActivity.this, DetailsActivity.class);
+                extras = new Bundle();
+
+                extras.putInt(DetailsActivity.EXRA_PDF_VALUES,4);
+                intent.putExtras(extras);
+             //   startActivity(intent);
+                break;
+
+            case R.id.txt_General_Registrar:
+                intent = new Intent(MainActivity.this, DetailsActivity.class);
+                extras = new Bundle();
+
+                extras.putInt(DetailsActivity.EXRA_PDF_VALUES,5);
+                intent.putExtras(extras);
+            //    startActivity(intent);
+                break;
+
+            case R.id.txt_Study_and_Examinations:
+                intent = new Intent(MainActivity.this, DetailsActivity.class);
+                extras = new Bundle();
+
+                extras.putInt(DetailsActivity.EXRA_PDF_VALUES,6);
+                intent.putExtras(extras);
+               // startActivity(intent);
+                break;
+
+            case R.id.txt_Admission_Section:
+                intent = new Intent(MainActivity.this, DetailsActivity.class);
+                extras = new Bundle();
+
+                extras.putInt(DetailsActivity.EXRA_PDF_VALUES,7);
+                intent.putExtras(extras);
+                startActivity(intent);
+                break;
+
+            case R.id.txt_Graduates:
+                intent = new Intent(MainActivity.this, DetailsActivity.class);
+                extras = new Bundle();
+
+                extras.putInt(DetailsActivity.EXRA_PDF_VALUES,8);
+                intent.putExtras(extras);
+                startActivity(intent);
+                break;
+
+            case R.id.txt_Student_Affairs:
+                intent = new Intent(MainActivity.this, DetailsActivity.class);
+                extras = new Bundle();
+
+                extras.putInt(DetailsActivity.EXRA_PDF_VALUES,9);
+                intent.putExtras(extras);
+                startActivity(intent);
+                break;
+
+            case R.id.txt_about_app:
+                intent = new Intent(MainActivity.this, DetailsActivity.class);
+                extras = new Bundle();
+
+                extras.putInt(DetailsActivity.EXRA_PDF_VALUES,10);
+                intent.putExtras(extras);
+                startActivity(intent);
+
+                break;
+
+
+
+
+        }
+        //close navigation drawer
+        mDrawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
 
 
 }
